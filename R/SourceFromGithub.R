363848c48c269f8df64ee3ca0265c6be23cec6ab
Avishai Wizel <Avishai987@gmail.com>
2022-10-30 15:13:23 +0200

    insert libs into the function

--- R/SourceFromGithub.R
+++ R/SourceFromGithub.R
@@ -1,6 +1,7 @@
-library(stringi)
-library(devtools)
 source_from_github <- function(user = "avishai987", repositoy, version, subfolder = "", script_name = "") {
+  require(stringi)
+  require(devtools)
+
   if (script_name == "") {script_name =  repositoy %s+% ".R"}
   else { script_name = gsub(pattern = " ", replacement = "%20", x = script_name)}
   url = "https://github.com/" %s+% user %s+% "/" %s+% repositoy %s+% "/blob/" %s+% version %s+%  "/" %s+% script_name %s+% "?raw=TRUE"
